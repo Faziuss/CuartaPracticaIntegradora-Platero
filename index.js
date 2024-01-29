e6c56b40889adc2bcbfb13c1c3b13d1f2608a698
Fabrizio Platero <fabriplatero88@gmail.com>
2024-01-29 19:38:30 -0300

    first commit

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ import app from "./src/app.js";
 app.listen(
   app.get("PORT") || 8080,
   () => {
-    console.log("Servidor listo y corriendo en el puerto 8080");
+    console.log("Servidor listo y corriendo en el puerto 8080 !");
   }
 );
