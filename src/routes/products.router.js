a5a1b70040e96a955532e8d65ca1b2d6d80be175
Fabrizio Platero <fabriplatero88@gmail.com>
2024-01-30 11:35:49 -0300

    added get pid endpoint for products

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,20 +1,43 @@
 import { Router } from "express";
-import fs from 'fs'
+import fs from "fs";
+import { uuid } from "uuidv4";
 
-const router = Router()
-const pathName  = "./products.json"
+const router = Router();
+const pathName = "./products.json";
 
-router.get('/', (req,res)=>{
-    fs.readFile(pathName, 'utf8', (err, data)=> {
-        if (err) {
-            console.log(err);
-            res.status(500).json({error: 'Internal server error'})
-            return;
-        }
+router.get("/", (req, res) => {
+  fs.readFile(pathName, "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send({ error: "Internal server error" });
+      return;
+    }
 
-        const products = JSON.parse(data)
-        res.json({products:products})
-    })
-}) 
+    const products = JSON.parse(data);
+    res.send({ status: "sucess", products: products });
+  });
+});
+
+router.get("/:pid", (req, res) => {
+  const pid = req.params.pid;
+
+  fs.readFile(pathName, "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send({ error: "Internal server error" });
+      return;
+    }
+
+    const products = JSON.parse(data);
+    const product = products.find(product => product.id == pid)
+
+    if(!product){
+        res.status(404).send({error: 'Product not found'})
+        return
+    }
+
+    res.send({ status: "sucess", products: product });
+  });
+});
 
 export default router;
