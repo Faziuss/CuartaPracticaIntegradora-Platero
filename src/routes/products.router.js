52191b2bae3bb14b947106e1a7098529edcc3dcb
Fabrizio Platero <fabriplatero88@gmail.com>
2024-01-30 12:15:30 -0300

    added POST endpoint to products

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -8,12 +8,16 @@ const pathName = "./products.json";
 router.get("/", (req, res) => {
   fs.readFile(pathName, "utf8", (err, data) => {
     if (err) {
-      console.log(err);
       res.status(500).send({ error: "Internal server error" });
       return;
     }
 
     const products = JSON.parse(data);
+    const limit = req.query.limit;
+
+    if (limit) {
+      products = products.slice(0, parseInt(limit, 10));
+    }
     res.send({ status: "sucess", products: products });
   });
 });
@@ -23,20 +27,86 @@ router.get("/:pid", (req, res) => {
 
   fs.readFile(pathName, "utf8", (err, data) => {
     if (err) {
-      console.log(err);
       res.status(500).send({ error: "Internal server error" });
       return;
     }
 
     const products = JSON.parse(data);
-    const product = products.find(product => product.id == pid)
+    const product = products.find((product) => product.id == pid);
 
-    if(!product){
-        res.status(404).send({error: 'Product not found'})
-        return
+    if (!product) {
+      res.status(404).send({ error: "Product not found" });
+      return;
     }
 
     res.send({ status: "sucess", products: product });
+  });
+});
+
+router.post("/", (req, res) => {
+  const newProd = req.body;
+
+  const allowedFields = [
+    "title",
+    "description",
+    "code",
+    "price",
+    "status",
+    "stock",
+    "category",
+    "thumbnails",
+  ];
+  const fields = Object.keys(newProd);
+  const disallowedFields = fields.filter(
+    (field) => !allowedFields.includes(field)
+  );
+
+  console.log(disallowedFields);
+
+  if (disallowedFields.length > 0) {
+    return res.status(400).send({
+      error: `Los siguientes campos NO estan permitidos: ${disallowedFields.join(
+        ", "
+      )}`,
+    });
+  }
+
+  if (
+    !newProd.title ||
+    !newProd.description ||
+    !newProd.code ||
+    !newProd.price ||
+    !newProd.stock ||
+    !newProd.category
+  ) {
+    return res.status(400).send({
+      error:
+        "Los campos title, description, code, price, stock, category SON OBLIGATORIOS",
+    });
+  }
+
+  if (newProd.status == undefined) {
+    newProd.status = true;
+  }
+  newProd.id = uuid();
+
+  fs.readFile(pathName, "utf8", (err, data) => {
+    if (err) {
+      res.status(500).send({ error: "Internal server error" });
+      return;
+    }
+
+    const products = JSON.parse(data);
+    products.push(newProd);
+
+    fs.writeFile(pathName, JSON.stringify(products), (err) => {
+      if (err) {
+        res.status(500).send({ error: "Internal server error" });
+        return;
+      }
+
+      res.send({ status: "sucess", message: "Nuevo producto agregado" });
+    });
   });
 });
 
