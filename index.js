918695b6fff4cad9d4ab543def372615ec3297bb
Fabrizio Platero <fabriplatero88@gmail.com>
2024-02-27 03:31:50 -0300

    separated fs on products to different a folder and added error handlers

--- index.js
+++
@@ -1,8 +0,0 @@
-import app from "./src/app.js";
-
-app.listen(
-  app.get("PORT") || 8080,
-  () => {
-    console.log("Servidor listo y corriendo en el puerto 8080 !");
-  }
-);
--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,215 +1,88 @@
 import { Router } from "express";
-import fs from "fs";
-import { v4 as uuidv4 } from "uuid";
-
+import Products from "../dao/fileManager/products.js";
+import { AppError } from "../helpers/AppError.js";
 const router = Router();
-const pathName = "./src/data/products.json";
+const manager = new Products();
 
-router.get("/", (req, res) => {
-  fs.readFile(pathName, "utf8", (err, data) => {
-    if (err) {
-      console.log(err);
-      return res.status(500).send({ error: "Internal server error" });
+router.get("/", async (req, res, next) => {
+  try {
+    const { limit } = req.query;
+
+    if (limit && (isNaN(limit) || Number(limit) < 0)) {
+      throw new AppError(400, { message: "Invalid limit query." });
     }
 
-    const products = JSON.parse(data);
-    const limit = req.query.limit;
+    const products = await manager.readProducts();
 
     if (limit) {
       products = products.slice(0, parseInt(limit, 10));
     }
     res.send({ status: "sucess", products: products });
-  });
+  } catch (error) {
+    return next(error);
+  }
 });
 
-router.get("/:pid", (req, res) => {
-  const {pid} = req.params
+router.get("/:pid", async (req, res, next) => {
+  try {
+    const { pid } = req.params;
 
-  fs.readFile(pathName, "utf8", (err, data) => {
-    if (err) {
-      return res.status(500).send({ error: "Internal server error" });
-    }
-
-    const products = JSON.parse(data);
-    const product = products.find((product) => product.id == pid);
+    const product = await manager.getProductById(pid);
 
     if (!product) {
-      return res.status(404).send({ error: "Product not found" });
+      return res.status(404).send({ error: "Producto no Encontrado" });
     }
 
     res.send({ status: "sucess", products: product });
-  });
+  } catch (error) {
+    return next(error);
+  }
 });
 
-router.post("/", (req, res) => {
-  const newProd = req.body;
+router.post("/", async (req, res, next) => {
+  try {
+    const io = app.get("io");
+    const body = req.body;
+    const newProd = await manager.addProduct(body);
 
-  const allowedFields = [
-    "title",
-    "description",
-    "code",
-    "price",
-    "status",
-    "stock",
-    "category",
-    "thumbnails",
-  ];
-  const fields = Object.keys(newProd);
-  const disallowedFields = fields.filter(
-    (field) => !allowedFields.includes(field)
-  );
+    io.emit("newProduct", newProd);
 
-  console.log(disallowedFields);
-
-  if (disallowedFields.length > 0) {
-    return res.status(400).send({
-      error: `Los siguientes campos NO estan permitidos: ${disallowedFields.join(
-        ", "
-      )}`,
-    });
+    res.send({ status: "sucess", message: "Nuevo producto agregado" });
+  } catch (error) {
+    return next(error);
   }
-
-  if (
-    !newProd.title ||
-    !newProd.description ||
-    !newProd.code ||
-    !newProd.price ||
-    !newProd.stock ||
-    !newProd.category
-  ) {
-    return res.status(400).send({
-      error:
-        "Los campos title, description, code, price, stock, category SON OBLIGATORIOS",
-    });
-  }
-
-  if (newProd.status == undefined) {
-    newProd.status = true;
-  }
-  newProd.id = uuidv4();
-
-  fs.readFile(pathName, "utf8", (err, data) => {
-    if (err) {
-      return res.status(500).send({ error: "Internal server error" });
-    }
-
-    const products = JSON.parse(data);
-    products.push(newProd);
-
-    const foundCode = products.some((p) => p.code === newProd.code);
-
-    if (foundCode) {
-      return res.status(400).send({
-        error: "El codigo identificador ya se encuentra en uso",
-      });
-    }
-
-    fs.writeFile(pathName, JSON.stringify(products), (err) => {
-      if (err) {
-        return res.status(500).send({ error: "Internal server error" });
-      }
-
-      res.send({ status: "sucess", message: "Nuevo producto agregado" });
-    });
-  });
 });
 
-router.put("/:pid", (req, res) => {
-  const upProd = req.body;
-  const {pid} = req.params
+router.put("/:pid", async (req, res, next) => {
+  try {
+    const upProd = req.body;
+    const { pid } = req.params;
 
-  const allowedFields = [
-    "title",
-    "description",
-    "code",
-    "price",
-    "status",
-    "stock",
-    "category",
-    "thumbnails",
-  ];
-  const fields = Object.keys(upProd);
-  const disallowedFields = fields.filter(
-    (field) => !allowedFields.includes(field)
-  );
+    await manager.updateProduct(pid, upProd);
 
-  if (disallowedFields.length > 0) {
-    return res.status(400).send({
-      error: `Los siguientes campos son inexistentes para la actualización: ${disallowedFields.join(
-        ", "
-      )}`,
+    res.send({
+      status: "sucess",
+      message: "Producto actualizado correctamente",
     });
+  } catch (error) {
+    return next(error);
   }
+});
 
-  fs.readFile(pathName, "utf8", (err, data) => {
-    if (err) {
-      return res.status(500).send({ error: "Internal server error" });
-    }
+router.delete("/:pid", async (req, res, next) => {
+  try {
+    const io = app.get("io");
+    const { pid } = req.params;
 
-    const products = JSON.parse(data);
-
-    const i = products.findIndex((product) => product.id == pid);
-
-    if (i === -1) {
-      return res.status(404).send({ error: "Producto no encontrado" });
-    }
-
-    if (upProd.code) {
-      const foundCode = products.some((p) => {
-        return p.code === upProd.code && p.id !== products[i].id;
-      });
-
-      if (foundCode) {
-        return res
-          .status(404)
-          .send({ error: "El codigo identificador ya se encuentra en uso" });
-      }
-    }
-
-    products[i] = { ...products[i], ...upProd };
-
-    fs.writeFile(pathName, JSON.stringify(products), (err) => {
-      if (err) {
-        return res.status(500).send({ error: "Internal server error" });
-      }
-
-      res.send({
-        status: "sucess",
-        message: "Producto actualizado correctamente",
-      });
+    await manager.deleteProduct(pid);
+    io.emit("deleteProduct", pid);
+    res.send({
+      status: "sucess",
+      message: "Producto eliminado existosamente",
     });
-  });
+  } catch (error) {
+    return next(error);
+  }
 });
 
 export default router;
-
-router.delete("/:pid", (req, res) => {
-  const {pid} = req.params
-
-  fs.readFile(pathName, "utf8", (err, data) => {
-    if (err) {
-      return res.status(500).send({ error: "Internal server error" });
-    }
-
-    const products = JSON.parse(data);
-
-    const i = products.findIndex((product) => product.id == pid);
-
-    if (i === -1) {
-      return res.status(404).send({ error: "Producto no encontrado" });
-    }
-
-    products.splice(i, 1);
-
-    fs.writeFile(pathName, JSON.stringify(products), (err) => {
-      if (err) {
-        return res.status(500).send({ error: "Internal server error" });
-      }
-
-      res.send({
-        status: "sucess",
-        message: "Producto eliminado existosamente",
-      });
-    });
-  });
-});
