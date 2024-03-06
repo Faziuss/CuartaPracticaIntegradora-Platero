da46d2ef2b8195b1c7f941b2e30602329cab49cb
Fabrizio Platero <fabriplatero88@gmail.com>
2024-03-05 22:37:55 -0300

    added carts views router

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,17 +1,19 @@
 import { Router } from "express";
 import Products from "../dao/fileManager/dbManagers/products.js";
+import Carts from "../dao/fileManager/dbManagers/carts.js";
 
 const router = Router();
-const manager = new Products();
+const managerProducts = new Products();
+const managerCarts = new Carts();
 
 router.get("/", async (_req, res) => {
-  const products = await manager.getProducts();
+  const products = await managerProducts.getProducts();
 
   return res.render("home", { products });
 });
 
 router.get("/realTimeProducts", async (_req, res) => {
-  const products = await manager.getProducts();
+  const products = await managerProducts.getProducts();
 
   return res.render("realTimeProducts", { products });
 });
@@ -21,11 +23,21 @@ router.get("/chat", async (_req, res) => {
 });
 
 router.get("/products", async (req, res) => {
-  const {docs, ...rest} = await manager.getProductsApi(req.query);
+  const { docs, ...rest } = await managerProducts.getProductsApi(req.query);
 
   const products = docs;
 
   return res.render("products", { products, ...rest });
+});
+
+router.get("/carts/:cid", async (req, res) => {
+  const { cid } = req.params;
+
+  const result = await managerCarts.getCartById(cid);
+
+  const carts = result.products;
+
+  return res.render("carts", { carts: carts.map((item) => item.toJSON()) });
 });
 
 export default router;
