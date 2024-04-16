14d4be9b046a2e173d0fc735434823e06d0e85f0
Fabrizio Platero <fabriplatero88@gmail.com>
2024-04-16 02:02:51 -0300

    Implemented layer architecture for products, separating it in controller, service and dao files

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -2,103 +2,18 @@ import { Router } from "express";
 import Products from "../dao/fileManager/dbManagers/products.js";
 import { AppError } from "../helpers/AppError.js";
 import mongoose from "mongoose";
+import ProductsController from "../controllers/products.controller.js";
 const router = Router();
 const manager = new Products();
 
-router.get("/", async (req, res, next) => {
-  try {
-    let products = await manager.getProductsApi(req.query);
+router.get("/", ProductsController.getProductsApi);
 
-    products.prevLink = products.hasPrevPage ? `${req.protocol}://${req.get('host')}/api/products?page=${products.prevPage}` : null;
-    products.nextLink = products.hasNextPage ? `${req.protocol}://${req.get('host')}/api/products?page=${products.nextPage}` : null;
+router.get("/:pid", ProductsController.getProductById);
 
+router.post("/", ProductsController.addProduct);
 
-    res.send({ status: "sucess", products });
-  } catch (error) {
-    return next(error);
-  }
-});
+router.put("/:pid", ProductsController.updateProduct);
 
-router.get("/:pid", async (req, res, next) => {
-  try {
-    const { pid } = req.params;
-    const isValid = mongoose.Types.ObjectId.isValid(pid);
-    if (!isValid) {
-      throw new AppError(400, { message: "El ID ingresado no es válido." });
-    }
-
-    const product = await manager.getProductById(pid);
-
-    if (!product) {
-      return res.status(404).send({ error: "Producto no Encontrado" });
-    }
-
-    res.send({ status: "sucess", products: product });
-  } catch (error) {
-    return next(error);
-  }
-});
-
-router.post("/", async (req, res, next) => {
-  try {
-    const io = req.app.get("io");
-    const body = req.body;
-    const newProd = await manager.addProduct(body);
-
-    io.emit("newProduct", newProd);
-
-    res.send({ status: "sucess", message: "Nuevo producto agregado" });
-  } catch (error) {
-    if (error instanceof mongoose.Error.ValidationError) {
-    }
-    return next(error);
-  }
-});
-
-router.put("/:pid", async (req, res, next) => {
-  try {
-    const upProd = req.body;
-    const { pid } = req.params;
-    const isValid = mongoose.Types.ObjectId.isValid(pid);
-    if (!isValid) {
-      throw new AppError(400, { message: "El ID ingresado no es válido." });
-    }
-
-    const product = await manager.updateProduct(pid, upProd);
-    if (!product) {
-      throw new AppError(404, { message: "Producto no encontrado." });
-    }
-
-    return res.send({
-      status: "sucess",
-      message: "Producto actualizado correctamente",
-    });
-  } catch (error) {
-    return next(error);
-  }
-});
-
-router.delete("/:pid", async (req, res, next) => {
-  try {
-    const io = req.app.get("io");
-    const { pid } = req.params;
-    const isValid = mongoose.Types.ObjectId.isValid(pid);
-    if (!isValid) {
-      throw new AppError(400, { message: "El ID ingresado no es válido." });
-    }
-
-    const product = await manager.deleteProduct(pid);
-    if (product.deletedCount === 0) {
-      throw new AppError(404, { message: "Producto no encontrado." });
-    }
-    io.emit("deleteProduct", pid);
-
-    return res
-      .status(200)
-      .json({ message: "Producto eliminado exitosamente." });
-  } catch (error) {
-    return next(error);
-  }
-});
+router.delete("/:pid", ProductsController.deleteProduct);
 
 export default router;
---
+++ src/services/products.service.js
@@ -0,0 +1,35 @@
+import Products from "../dao/fileManager/dbManagers/products.js";
+import { AppError } from "../helpers/AppError.js";
+
+class ProductService {
+    constructor() {
+      this.dao = new Products();
+    }
+
+    async getProductsApi(query){
+        return await this.dao.getProductsApi(query);
+    }
+
+    async getProductById(pid){
+        const product = await this.dao.getProductById(pid);
+        
+        if (!product) {
+            throw new AppError(404,{ message: "Producto no Encontrado" });
+        }
+        return product
+    }
+    async addProduct(body){
+        return this.dao.addProduct(body);
+    }
+    async updateProduct(pid, upProd){
+        const product = await this.dao.updateProduct(pid, upProd);
+            if (!product) {
+              throw new AppError(404, { message: "Producto no encontrado." });
+            }
+    }
+    async deleteProduct(pid){
+        return await this.dao.deleteProduct(pid);
+    }
+}
+
+export default ProductService
