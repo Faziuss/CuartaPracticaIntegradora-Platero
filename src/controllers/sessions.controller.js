d7b779fbfe8e2867a915b213e5277874b6e6c18e
Fabrizio Platero <fabriplatero88@gmail.com>
2024-04-16 12:23:17 -0300

    Created Sessions controller to handle authentication sessions

---
+++ src/controllers/sessions.controller.js
@@ -0,0 +1,61 @@
+import { AppError } from "../helpers/AppError.js"
+
+class SessionsController {
+    static async register(req,res){
+        res.status(201).send({ status: "sucess", message: "Usuario Registrado"})
+    }
+
+    static async registerFail(req,res){
+        throw new AppError(401, { message: "Authentication Error" })
+    }
+
+    static async login(req,res){
+    const user = req.user;
+
+    req.session.user = {
+      name: `${user.first_name} ${user.last_name}`,
+      email: user.email,
+      age: user.age,
+      roles: user.roles,
+      cart: user.cart,
+    };
+    res.status(201).send({
+      status: "sucess",
+      payload: req.session.user,
+      message: "Logeado Correctamente",
+    });
+    }
+
+    static async loginFail(req,res){
+        res.status(401).send({ status: "error", error: "login fail" });
+    }
+
+    static async githubCallback(req,res){
+    const user = req.user;
+
+    req.session.user = {
+      name: `${user.first_name} ${user.last_name}`,
+      email: user.email,
+      age: user.age,
+      roles: user.roles,
+      cart: user.cart,
+    };
+    res.redirect("/products");
+    }
+
+    static async logOut(req,res){
+        req.session.destroy((err) => {
+            if (err)
+              throw new AppError(500, {
+                message: "Hubo un error al intentar destruir la session",
+              });
+          });
+          res.redirect("/login");
+    }
+
+    static async current(req,res){
+        res.send({user: req.session.user})
+    }
+}
+
+export default SessionsController
--- src/routes/sessions.router.js
+++ src/routes/sessions.router.js
@@ -1,58 +1,27 @@
 import { Router } from "express";
-import { AppError } from "../helpers/AppError.js";
 import passport from "passport";
+import SessionsController from "../controllers/sessions.controller.js";
+import privateAcess from "../middlewares/privateAcess.js";
 
 const sessionRouter = Router();
-
-const privateAcess = (req, res, next) => {
-  if (!req.session.user) {
-    console.log("not logged in");
-    return res.redirect("/login");
-  }
-
-  next();
-};
 
 sessionRouter.post(
   "/register",
   passport.authenticate("register", {
     failureRedirect: "api/sessions/registerFail",
-  }),
-  async (req, res) => {
-    res.status(201).send({ status: "sucess", message: "Usuario Registrado" });
-  }
+  }), SessionsController.register
 );
 
-sessionRouter.post("/registerFail", (req, res) => {
-  throw new AppError(401, { message: "Authentication Error" });
-});
+sessionRouter.post("/registerFail", SessionsController.registerFail);
 
 sessionRouter.post(
   "/login",
   passport.authenticate("login", {
     failureRedirect: "/api/sessions/loginFail",
-  }),
-  async (req, res) => {
-    const user = req.user;
-
-    req.session.user = {
-      name: `${user.first_name} ${user.last_name}`,
-      email: user.email,
-      age: user.age,
-      roles: user.roles,
-      cart: user.cart,
-    };
-    res.status(201).send({
-      status: "sucess",
-      payload: req.session.user,
-      message: "Logeado Correctamente",
-    });
-  }
+  }), SessionsController.login
 );
 
-sessionRouter.get("/loginFail", (req, res) => {
-  res.status(401).send({ status: "error", error: "login fail" });
-});
+sessionRouter.get("/loginFail", SessionsController.loginFail);
 
 sessionRouter.get(
   "/github",
@@ -62,33 +31,11 @@ sessionRouter.get(
 
 sessionRouter.get(
   "/githubcallback",
-  passport.authenticate("github", { failureRedirect: "/login" }),
-  async (req, res) => {
-    const user = req.user;
-
-    req.session.user = {
-      name: `${user.first_name} ${user.last_name}`,
-      email: user.email,
-      age: user.age,
-      roles: user.roles,
-      cart: user.cart,
-    };
-    res.redirect("/products");
-  }
+  passport.authenticate("github", { failureRedirect: "/login" }), SessionsController.githubCallback
 );
 
-sessionRouter.get("/logout", async (req, res) => {
-  req.session.destroy((err) => {
-    if (err)
-      throw new AppError(500, {
-        message: "Hubo un error al intentar destruir la session",
-      });
-  });
-  res.redirect("/login");
-});
+sessionRouter.get("/logout", SessionsController.logOut);
 
-sessionRouter.get("/current", privateAcess, async (req, res) => {
-   res.send({user: req.session.user})
-});
+sessionRouter.get("/current", privateAcess, SessionsController.current);
 
 export default sessionRouter;
