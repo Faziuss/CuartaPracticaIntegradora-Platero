b652937fe579b7d5bb15d634425e514db8a6e349
Fabrizio Platero <fabriplatero88@gmail.com>
2024-04-16 12:41:39 -0300

    removed old imports

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,10 +1,6 @@
 import { Router } from "express";
-import Products from "../dao/fileManager/dbManagers/products.js";
-import { AppError } from "../helpers/AppError.js";
-import mongoose from "mongoose";
 import ProductsController from "../controllers/products.controller.js";
 const router = Router();
-const manager = new Products();
 
 router.get("/", ProductsController.getProductsApi);
 
