4be35267ab80b6be8813e8453a1e5d7540b54f78
Fabrizio Platero <fabriplatero88@gmail.com>
2024-04-27 18:01:49 -0300

    added ticket model

---
+++ src/dao/mongo/models/ticket.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const ticketSchema = new mongoose.Schema({
+  code: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  purchase_datetime: {
+    type: String,
+    default: new Date().toLocaleTimeString(),
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  purchaser: { type: String, required: true },
+});
+
+const ticketModel = mongoose.model("ticket", ticketSchema)
+export default ticketModel
