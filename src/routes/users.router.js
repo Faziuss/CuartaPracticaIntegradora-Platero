e670e5661bcf23febcbb629c7310956ce8a0de03
Fabrizio Platero <fabriplatero88@gmail.com>
2024-06-13 22:20:44 -0300

    added routers for documents and profile-picture upload

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -1,8 +1,11 @@
 import Router from "express";
 import UsersController from "../controllers/user.controller.js";
 import { roleAdmin } from "../middlewares/roleAcess.js";
+import upload from "../middlewares/upload.middleware.js";
 const usersRouter = Router();
 
 usersRouter.get("/premium/:uid", roleAdmin, UsersController.changeRole);
+usersRouter.post('/:uid/documents', upload.array('document') ,UsersController.uploadDocuments)
+usersRouter.post('/:uid/profile-picture', upload.single('profile'), UsersController.uploadProfilePicture)
 
 export default usersRouter;
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,39 +1,95 @@
 class UsersService {
-    constructor(dao){
-        this.dao = dao;
+  constructor(dao) {
+    this.dao = dao;
+  }
+
+  async getAll(queryParams = null) {
+    return await this.dao.getAll();
+  }
+
+  async getById(id) {
+    const item = await this.dao.getById(id);
+    if (!item) throw { message: `There's no Item by id ${id}`, status: 400 };
+    return item;
+  }
+
+  async getByProperty(property, value) {
+    const item = await this.dao.getByProperty(property, value);
+    if (!item)
+      throw {
+        message: `There's no Item by ${property} = ${value}`,
+        status: 400,
+      };
+    return item;
+  }
+
+  async create(toy) {
+    return await this.dao.create(toy);
+  }
+
+  async update(id, toy) {
+    console.log("---->", id, toy);
+    await this.dao.getById(id);
+    return await this.dao.update(id, toy);
+  }
+
+  async delete(id) {
+    await this.dao.getById(id);
+    return await this.dao.delete(id);
+  }
+
+  async addDocuments(id, files) {
+    const user = await this.getById(id);
+    let documents = user.documents || [];
+
+    documents = [
+      ...documents,
+      ...files.map((file) => {
+        return {
+          name: file.originalname,
+          reference: file.path.split("public")[1].replace(/\\/g, "/"),
+        };
+      }),
+    ];
+
+    return await this.update(id, { documents: documents });
+  }
+  async addProfilePicture(id, file) {
+    await this.getById(id);
+    return await this.update(id, {
+      profile_picture: file.path.split("public")[1].replace(/\\/g, "/"),
+    });
+  }
+
+  async changeRole(uid) {
+    const user = await this.getById(uid);
+    if (user.roles == "Usuario") {
+      if (!user.documents.some((d) => d.name.includes("Identificacion"))) {
+        throw new Error("Faltan documentos");
+      }
+      if (
+        !user.documents.some((d) => d.name.includes("Comprobante de domicilio"))
+      ) {
+        throw new Error("Faltan documentos");
+      }
+      if (
+        !user.documents.some((d) =>
+          d.name.includes("Comprobante de estado de cuenta")
+        )
+      ) {
+        throw new Error("Faltan documentos");
+      }
     }
 
-    async getAll(queryParams = null){
-        return await this.dao.getAll()
+    if (!["Usuario", "Premium"].includes(user.roles)) {
+      throw new Error("User has invalid role");
     }
 
-    async getById(id){    
-        const item = await this.dao.getById(id); 
-        if(!item) throw { message:`There's no Item by id ${id}`, status:400 }
-        return item;
-    }
+    user.roles = user.roles == "Usuario" ? "Premium" : "Usuario";
 
-    async getByProperty(property, value){
-        const item = await this.dao.getByProperty(property, value); 
-        if(!item) throw { message:`There's no Item by ${property} = ${value}`, status:400 }
-        return item;
-    }
-
-
-    async create(toy){
-        return await this.dao.create(toy);
-    }
-
-    async update(id, toy){
-        console.log('---->', id, toy)
-        await this.dao.getById(id);
-        return await this.dao.update(id, toy);
-    }
-
-    async delete(id){
-        await this.dao.getById(id);
-        return await this.dao.delete(id);
-    }
+    await this.update(user._id.toString(), { $set: { role: user.roles } });
+    return await this.getById(uid);
+  }
 }
 
-export default UsersService
+export default UsersService;
